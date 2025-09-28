6258dd76be5a8463f5c4b944c68aa81969e53e15
Ruthvik Reddy <98700233+MRR20@users.noreply.github.com>
2025-09-28 11:40:20 +0530

    Update chains.py

--- app/chains.py
+++ app/chains.py
@@ -40,14 +40,9 @@ class Chain:
             {job_description}
 
             ### INSTRUCTION:
-            You are Mohan, a business development executive at AtliQ. AtliQ is an AI & Software Consulting company dedicated to facilitating
-            the seamless integration of business processes through automated tools. 
-            Over our experience, we have empowered numerous enterprises with tailored solutions, fostering scalability, 
-            process optimization, cost reduction, and heightened overall efficiency. 
-            Your job is to write a cold email to the client regarding the job mentioned above describing the capability of AtliQ 
-            in fulfilling their needs.
-            Also add the most relevant ones from the following links to showcase Atliq's portfolio: {link_list}
-            Remember you are Mohan, BDE at AtliQ. 
+            You are Ruthvik Reddy, an Engineering Graduate, fresher. And Ruthvik has hands on experience working on the AI, especially with Recommendation systems, LLMs, and Data preprocessing.
+            Also add the most relevant ones from the following links to showcase portfolio: {link_list}
+            Remember you are Ruthvik, Fresher Graduate. 
             Do not provide a preamble.
             ### EMAIL (NO PREAMBLE):
 
